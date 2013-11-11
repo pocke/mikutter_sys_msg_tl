534db0d43a5638c24d452a2e50d6066c280ffd4d
pocke <p.ck.t22@gmail.com>
2013-11-11 14:28:38 +0900

    add license

--- sys_msg_tl.rb
+++ sys_msg_tl.rb
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
-Plugin.create(:sys_msg_tl) do
+# Copyright 2013, pocket
+# Licensed MIT
+# http://opensource.org/licenses/mit-license.php
 
+Plugin.create(:sys_msg_tl) do
   tab :sys_msg, "system messages" do
     set_icon MUI::Skin.get("icon.png")
     timeline :sys_msg
